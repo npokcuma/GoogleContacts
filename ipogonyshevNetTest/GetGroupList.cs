85a4405a2da93e8e55f89b6f07b83e08f0ec2b9f
npokcuma <nail-it@mail.ru>
2019-08-24 13:50:55 +0700

    - Добавил в игнор-лист .vs, папку packages. Удалил лишние игнор-листы.

---
+++ ipogonyshevNetTest/GetGroupList.cs
@@ -0,0 +1,30 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.PeopleService.v1;
+using Google.Apis.PeopleService.v1.Data;
+using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace ipogonyshevNetTest
+{
+    class GetGroupList:GroupData
+    {
+        public static List<GroupData> Getlist(PeopleServiceService service)
+        {
+
+            ContactGroupsResource groupsResource = new ContactGroupsResource(service);
+            ContactGroupsResource.ListRequest listRequest = groupsResource.List();
+            ListContactGroupsResponse response = listRequest.Execute();
+
+            //eg to show name of each group
+            List<GroupData> groupNames = new List<GroupData>();
+            foreach (ContactGroup group in response.ContactGroups)
+            {
+                groupNames.Add(new GroupData() { GroupName = group.FormattedName,GroupId=group.ResourceName,GroupMemberCount=group.MemberCount });
+            }
+            return groupNames;
+        }
+    }
+}
+
