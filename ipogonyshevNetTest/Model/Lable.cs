ef00ba90e9ac9e12c331cb5f5456226aafa6a933
Hiryu70 <che.maxv@gmail.com>
2019-08-28 19:26:47 +0300

    - Исправлено Id групп не уникальные

--- ipogonyshevNetTest/Model/Lable.cs
+++ ipogonyshevNetTest/Model/Lable.cs
@@ -1,9 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ipogonyshevNetTest.Model
 {
 	public class Lable
 	{
+		public Lable()
+		{
+			Id = Guid.NewGuid().ToString();
+		}
+
+
 		public string Id { get; set; }
 
 		public string Name { get; set; }
